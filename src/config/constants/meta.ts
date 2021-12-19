89b842a57cd342a40cc5c11b37c5c5609fce2587
BlockChainSuperDev <leyr1114@gmail.com>
2021-12-20 02:09:42 +1000

    commit

--- src/config/constants/meta.ts
+++ src/config/constants/meta.ts
@@ -2,10 +2,10 @@ import { ContextApi } from 'contexts/Localization/types'
 import { PageMeta } from './types'
 
 export const DEFAULT_META: PageMeta = {
-  title: 'NSBC',
+  title: 'NSAVDEX',
   description:
-    'The most popular AMM on BSC by user count! Earn CAKE through yield farming or win it in the Lottery, then stake it in Syrup Pools to earn more tokens! Initial Farm Offerings (new token launch model pioneered by NSBC), NFTs, and more, on a platform you can trust.',
-  image: 'https://NSBC.finance/images/hero.png',
+    'The best AMM on Binance! Earn NSBC through yield farming, staking or win the lottery and prediction. Join the referral program. NFT is coming soon...',
+  image: 'https://nsavdex.io/images/hero.png',
 }
 
 export const getCustomMeta = (path: string, t: ContextApi['t']): PageMeta => {
